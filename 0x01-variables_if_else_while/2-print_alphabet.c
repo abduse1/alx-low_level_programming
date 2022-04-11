a4be71fd03b59ae8d7e1baca9e80ffd5a61b0a6c
abduse1 <abduselamed12@gmail.com>
2022-04-11 04:19:52 -0700

    my commit message for task two

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main entry
+ * return 0 for success
+ */
+
+int main (void)
+{
+ for (int i = 97; i < 123; i++)
+{
+putchar(i);
+putchar("\n");
+}
+return (0);
+}
