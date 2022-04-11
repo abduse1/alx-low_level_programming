908e941094207045409cbd224a06d324868f84ed
abduse1 <abduselamed12@gmail.com>
2022-04-11 01:51:44 -0700

    My second commit message

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,10 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/** if the last digit of n is greater than 5: the string and is greater than 5 
+/** 
+ * if the last digit of n is greater than 5 
  * if the last digit of n is 0: the string and is 0
- * if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
- * main entry
+ * if the last digit of n is less than 6 and not 0
+ * main - assign a random number to the variable n each time it is executed
  * return 0 (success)
  */
 int main(void)
@@ -24,7 +25,7 @@ else if (last_digit == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
-else if(last_digit < 6 && last_digit != 0)
+else if (last_digit < 6 && last_digit != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
