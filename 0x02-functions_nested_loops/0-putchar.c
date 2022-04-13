165012c6638f9b29217581c3fadf8ce0b653a0af
abduse1 <abduselamed12@gmail.com>
2022-04-13 14:07:57 -0700

    My commit to task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 #include "main.h"
-=======
-#include "main."
->>>>>>> 144c852cf2f48cf2c3e38288f97f9e05b9323f35
 
 /**
  * main - program that prints Holberton.
@@ -12,27 +8,14 @@
 
 int main(void)
 {
-<<<<<<< HEAD
-	int n = 1;
+	int n = 0;
 	char str_put[8] = "_putchar";
 
-	while (n < 9)
+	while (n < 8)
 	{
 		_putchar(str_put[n]);
 		n += 1;
 	}
 	_putchar('\n');
 	return (0);
-=======
-_putchar(m);
-_putchar(n);
-_putchar(o);
-_putchar(q);
-_putchar(s);
-_putchar(v);
-_putchar(w);
-_putchar(y);
-_putchar('\n');
-return (0);
->>>>>>> 144c852cf2f48cf2c3e38288f97f9e05b9323f35
 }
--- 0x02-functions_nested_loops/READADME.md
+++
@@ -1,1 +0,0 @@
-My Readme message to 0x02-functions_nested_loops
