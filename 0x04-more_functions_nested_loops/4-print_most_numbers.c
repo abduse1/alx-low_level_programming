327c6e1ff26fb1d69c6041de9676baa88a915460
abduse1 <abduselamed12@gmail.com>
2022-04-17 15:06:14 -0700

    My commit message to task 2

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,13 +2,14 @@
 
 /** print_most_numbers - Print the numbers since 0 up to 9
  *
- *
+ * Prints the numbers excluding 2 and 4
  * Return: The numbers since 0 up to 9
  */
 
 void print_most_numbers(void)
 {
 int n;
+
 for (n = 0; n <= 9; n++)
 {
 	if ((n == 2) || (n == 4))
@@ -20,5 +21,5 @@ for (n = 0; n <= 9; n++)
 		_putchar(n + '0');
 	}
 }
-_putchar('\n');
+	_putchar('\n');
 }
