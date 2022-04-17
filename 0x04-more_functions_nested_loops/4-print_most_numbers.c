9100a274672aa4a610bbed60a9465c8dbcc768ef
abduse1 <abduselamed12@gmail.com>
2022-04-17 14:24:10 -0700

    My commit message to task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+if (((n >= 0) && (n < 2)) || ((n > 4) && (n <= 9)))
+{
+_putchar(n + '0');
+}
+_putchar('\n');
+}
