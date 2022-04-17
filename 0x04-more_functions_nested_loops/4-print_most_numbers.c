52ef900d3ce2ecd2b0ad32ae09d115246222006c
abduse1 <abduselamed12@gmail.com>
2022-04-17 15:01:33 -0700

    My commit message to task 2

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,13 @@ void print_most_numbers(void)
 int n;
 for (n = 0; n <= 9; n++)
 {
-	if (((n >= 0) && (n < 2)) || ((n > 4) && (n <= 9)))
+	if ((n == 2) || (n == 4))
+	{ 
+		continue;
+	}
+	else
 	{
-	_putchar(n + '0');
+		_putchar(n + '0');
 	}
 }
 _putchar('\n');
