72a5fda2a6f1cac0c529e5e73afbde7f15a4a6b2
abduse1 <abduselamed12@gmail.com>
2022-04-20 14:09:51 -0700

    My commit message to task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,14 @@
 #include "main.h"
-/** 
- * print_rev - prints a string in reverse 
+
+/**
+ * print_rev - prints a string in reverse
  *
  * returns 0: sucess
  */
 void print_rev(char *s)
 {
 	int c = 0;
+
 	while (s[c] != 0)
 	{
 		c++;
