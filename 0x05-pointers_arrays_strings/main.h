6bc3142e8a065bf402aa2a65a65a1bc1448d0a25
abduse1 <abduselamed12@gmail.com>
2022-04-20 01:18:07 -0700

    My commit message to task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef <MAIN.H>
-#define <MAIN.H>
-
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
@@ -12,18 +9,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-#endif
-
-
-
-
-
-
-
-
-
-
+int _atoi(char *s)
 
 
 
