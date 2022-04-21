7b93b22b910ece90866094cb0104118b0507a843
abduse1 <abduselamed12@gmail.com>
2022-04-21 01:43:10 -0700

    My commit message to task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * _strcat - concenates two string variables
+ * @dest : the destination string
+ * @src : the source string
+ *
+ * Return : the pointer to dest after concatenation
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dstlen = 0;
+	int i;
+
+	while(dest[dstlen])
+	{
+		dstlen++;
+	}
+	for(i = 0; src[i] != '\0'; i++)
+	{
+		dest[dstlen] = src[i];
+	}
+
+	dest[dstlen] = '\0';
+	return (dest);
+}
