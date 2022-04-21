a44e6fe4b8e06452d2231a1e27d50e4c9e61aa20
abduse1 <abduselamed12@gmail.com>
2022-04-21 04:13:24 -0700

    My commit message to task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * _strncat - to concatenates limited number of strings 
+ * @dest: destination string 
+ * @src: source string  
+ * @n: limit tha length of source string
+ *
+ * Return : pointer  to destination string after concatenation
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i;
+	while(dest[dest_len])
+	{
+		dest_len++;
+	}
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
