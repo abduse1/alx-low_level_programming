046e403a5f68d24aa87b462cf3128190767bd04d
abduse1 <abduselamed12@gmail.com>
2022-04-21 12:26:41 -0700

    My commit message to task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _srncmp - compares two strings
+ * @s1 - string 1
+ * @s2 _ string 2
+ *
+ * Returns : integer 
+ */
+int _strcmp(char *s1, char *s2);
+{
+	int i = 0, j = 0, k = 0, n = 0, tmp;
+
+	while (s1[i])
+	{
+		i++;
+	}
+
+	while (s2[j])
+        {
+                j++;
+        }
+
+	if (i <= j)
+	{
+		tmp = i;
+	}
+	else
+	{
+		tmp = j;
+	}
+	while (c <= tmp)
+	{
+		if (s1[i] == s2[j])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			n = s1[i] - s2[j];
+			break;
+		}
+
+		c++;
+	}
+
+	return (r);
+}
+
+
+
+
+
