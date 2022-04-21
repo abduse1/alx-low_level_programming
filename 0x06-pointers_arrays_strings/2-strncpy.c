5375c449bd2888566d06c4fc4eec3e4116aafb0a
abduse1 <abduselamed12@gmail.com>
2022-04-21 04:46:18 -0700

    My commit message to task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,17 +4,31 @@
  * _strncpy - copy strings
  * @dest: destination string 
  * @src: source string
+ * @n: limit of copy
  *
  * Return: pointer of the destinationa after copy
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-                   dest[i] = src[i];
-	for ( ; i < n; i++)
-                   dest[i] = '\0'
+	int i = 0, j = 0;
 
-	return dest;
+	while (src[j])
+	{
+		j++;
+	}
+
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 }
