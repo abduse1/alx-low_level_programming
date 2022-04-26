58eca3aa05307cbf7d6ba838f749b447938a1a8a
abduse1 <abduselamed12@gmail.com>
2022-04-26 02:28:29 -0700

    My commit message to task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,12 +4,14 @@
  * _memcpy - copy memory area from src dest
  * @dest: destination memory area
  * @src: source memory area
+ * @n: number of bytes from source to be copied
  *
  * Return: dest memory area after replacement
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
+
 	for(i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
