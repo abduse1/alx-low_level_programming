4589bc7183059cbf05edea3b62da82a299581cd1
abduse1 <abduselamed12@gmail.com>
2022-05-11 04:51:45 -0700

    My commit message to task 5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * new_dog - new attributes to a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: always nothing
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog != NULL)
+	{
+		new_dog->name;
+		new_dog->age;
+		new_dog->owner;
+	}
+}
