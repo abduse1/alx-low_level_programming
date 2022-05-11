ddfaaf08101d7462fd36e0e3204f1b30ca4edc85
abduse1 <abduselamed12@gmail.com>
2022-05-11 01:24:02 -0700

    My commit message to task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,17 +1,21 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- *init_dog - to intialize struct dog
- * @d: ..
- * @name: ..
- * @age: ..
- * @owner: ..
- */
+  * init_dog - Initializes a dog structure
+  * @d: A dog structure.
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
+  * Return: Nothing
+  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-struct dog my_dog;
-d = &my_dog;
-my_dog.name;
-my_dog.float;
-my_dog.owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
