48ba7a9ec48fe98c031682bfca82e8a3cf7ccf97
abduse1 <abduselamed12@gmail.com>
2022-05-12 01:46:54 -0700

    My commit message to task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function executes elements of array
+ * @array: array to be iterated
+ *@size: the size of an array
+ *@action: pointer to function array_iterator
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int)
+{
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
