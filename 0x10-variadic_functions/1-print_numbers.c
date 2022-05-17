2c9ec2d7e5b1c6c3fd15875a32ed0d20a09a29d0
abduse1 <abduselamed12@gmail.com>
2022-05-17 01:38:24 -0700

    Prints numbers passed to variadic functions with separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-#include <stdarg.h>
+#include <stdlib.h>
 
 /**
  *print_numbers - prints numbers followed by a new line
@@ -9,21 +9,25 @@
  *
  *Return: void
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list nlist;
-	unsigned int i;
-	int prints;
+	va_list args;
+	unsigned int i = 0;
 
-	va_start(nlist, n);
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		prints = va_arg(nlist, int);
-		printf("%d", prints);
-		if (separator && i < (n - 1))
-			printf("%s", separator);
+		va_start(args, n);
+
+		while (i < n)
+		{
+			printf("%d", va_arg(args, int));
+
+			if (i != n - 1  && separator != NULL)
+				printf("%s", separator);
+
+			i++;
+		}
+		va_end(args);
 	}
-	va_end(nlist);
 	printf("\n");
 }
