a3c3e3231d9610ae451874b60207e298191ee65a
abduse1 <abduselamed12@gmail.com>
2022-05-17 00:31:04 -0700

    The sum of all parameters passed to variadic function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,23 +8,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+
 	va_list args;
-	unsigned int i = 0;
-	int sum = 0;
+	unsigned int i;
+	int res_sum;
 
-	if (n != 0)
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+	res_sum = 0;
+	for (i = 0; i < n; i++)
 	{
-		va_start(args, n);
-
-		while (i < n)
-		{
-			sum += va_arg(args, int);
-			i++;
-		}
-
-		va_end(args);
-		return (sum);
+		res_sum += va_arg(args, int);
 	}
+	va_end(args);
 
-	return (0);
+	return (res_sum);
 }
+
